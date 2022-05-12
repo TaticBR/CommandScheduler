e288230d408b24f9042cf782e9e0f71b0a53621e
Karan Melo <karan_melo@hotmail.com>
2022-05-12 19:52:49 -0300

    feat: add sync option with false as default value

--- src/commandhandling/CommandOpts.ts
+++ src/commandhandling/CommandOpts.ts
@@ -1,14 +1,15 @@
 export class CommandOpts {
+  retryOnExceptions?: any[] = [Error];
 
-    retryOnExceptions?: any[] = [Error];
+  retryCount?: number = 1;
 
-    retryCount?: number = 1;
+  retryDelaySeconds?: number = 1;
 
-    retryDelaySeconds?: number = 1;
+  sync?: boolean = false;
 
-    onSuccess?: (jobName: any, jobArgs: any[]) => Promise<void> | void;
+  onSuccess?: (jobName: any, jobArgs: any[]) => Promise<void> | void;
 
-    onFailed?: (jobName: any, jobArgs: any[], err: any) => Promise<void> | void;
+  onFailed?: (jobName: any, jobArgs: any[], err: any) => Promise<void> | void;
 
-    onError?: (jobName: any, jobArgs: any[], err: any) => Promise<void> | void;
+  onError?: (jobName: any, jobArgs: any[], err: any) => Promise<void> | void;
 }
